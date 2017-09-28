24cdbb0360497820142b9d3992019b28df922c77
Rui Baiao <rui@frostdigital.se>
2017-09-28 11:10:42 +0200

    cleanup

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,2 @@
 export * from './Button';
-// export * from './Card';
 export * from './CardSection';
-// export * from './Header';
-// export * from './TextField';
-// export * from './Spinner';
-// export * from './ConfirmationModal';
